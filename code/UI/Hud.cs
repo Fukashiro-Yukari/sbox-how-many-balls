997721f5b1bf1a48f016ab85f6562d828a5abbd1
Neptune QTG <Neptuneqtg@gmail.com>
2021-08-17 22:18:38 +0800

    Added ChatBox and VoiceList and Scoreboard

--- code/UI/Hud.cs
+++ code/UI/Hud.cs
@@ -12,5 +12,8 @@ public partial class HowManyBallsHud : HudEntity<RootPanel>
         RootPanel.StyleSheet.Load("/UI/Hud.scss");
 
         RootPanel.AddChild<BallsAmount>();
+        RootPanel.AddChild<ChatBox>();
+        RootPanel.AddChild<VoiceList>();
+        RootPanel.AddChild<Scoreboard<ScoreboardEntry>>();
     }
 }
